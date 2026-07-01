51f35ec5a641a9e06e71df2bc6e8a4ec396f66c2
Iman Hameed <imanhameed167@gmail.com>
2026-07-01 11:48:19 +0500

    Update Bill Calculator.cpp

--- Bill Calculator.cpp
+++ Bill Calculator.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int units, TV, paper,fcharge;
+	//M=METER
+	int units, TV, paper,Mcharge;
+	float F_P_adjustment,FC_surcharge,electricity_duty,R_S_tax,ED_on_FPA;
 	float bill;
 	float discount;
 	cout<<"enter electricity bills units:";
@@ -10,8 +12,19 @@ int main(){
 	cin>>TV;
 	cout<<"enter paper fund:";
 	cin>>paper;
-	cout<<"enter fix charges:";
-	cin>>fcharge;
+	cout<<"enter meter charges:";
+	cin>>Mcharge;
+	cout<<"enter fuel price adjustment:";
+	cin>>F_P_adjustment;
+	cout<<"enter F.C surcharge:";
+	cin>>FC_surcharge;
+	cout<<"enter electricity duty charge:";
+	cin>>electricity_duty;
+    cout<<"enter R_S_tax:";
+    cin>>R_S_tax;
+    cout<<"enter ED on FPA:";
+    cin>>ED_on_FPA;
+	
 	//bill=units*50;
 	if(units>500){
 	bill=units*50;
@@ -23,14 +36,12 @@ int main(){
 	bill=units*20;
 }
 	
-	bill=bill+(TV+paper+fcharge);
+	bill=bill+(TV+paper+Mcharge+F_P_adjustment+FC_surcharge+electricity_duty+R_S_tax+ED_on_FPA);
 	cout<<"total bill="<<bill<<endl;
-	if(bill<1500){
+	if(bill<2500){
 		discount=bill-1500/100;
 		cout<<"you get discount!"<<discount<<endl;
 	}
 	
 	return 0;
-	
 }
-
